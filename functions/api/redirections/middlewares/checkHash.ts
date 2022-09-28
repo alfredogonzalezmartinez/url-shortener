02918ca425ac8236a3529c01353bac16cbf10944
Alfredo González Martínez <48945157+alfredogonzalezmartinez@users.noreply.github.com>
2022-09-28 11:39:35 +0200

    Add hash filter to avoid invalid characters

--- functions/api/redirections/middlewares/checkHash.ts
+++ functions/api/redirections/middlewares/checkHash.ts
@@ -1,11 +1,22 @@
 import { ERROR_STATUS, errorResponse } from "../helpers/responseWrappers";
 
+const INVALID_CHARACTERS = [" ", "/", "\\"];
+
 export const checkHash: PagesFunction<
   Bindings,
   any,
   { hash?: Hash; isFreeHash?: IsFreeHash }
 > = async ({ data, env, request, next }) => {
   const hash = data.hash as string;
+
+  for (const character of INVALID_CHARACTERS) {
+    if (hash.includes(character)) {
+      return errorResponse(
+        `Invalid character "${character}" in hash`,
+        ERROR_STATUS.BAD_REQUEST
+      );
+    }
+  }
 
   const isFreeHash = (await env.REDIRECTIONS?.get(hash)) == null;
 
